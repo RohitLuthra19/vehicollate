fbf3f887959ae679475fb2303d3abf704b1ba99d
Rohit Luthra <rohit.luthra19@gmail.com>
2020-07-25 13:36:00 +0530

    removed unused code

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
       initialRoute: '/',
       routes: {
         '/': (context) => Login(),
